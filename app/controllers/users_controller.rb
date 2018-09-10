f8ee39e5480e90b01e11cfe1dd1ca5339eb64fca
oleksandrkits <oleksandrkit@tahometer.com>
2018-09-10 15:27:18 +0300

    All users page access is now aviable only for admin. User now can view only his own profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,75 +1,28 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :is_admin, only: [:index]
 
   def index
     @users = User.all
   end
 
-  def new
-    @user = User.new
-    @user.adress = Adress.new
-  end
-
-  def edit
-  end
-
   def show
-    @img = Image.find(Image.ids.sample)
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    id = current_user.try(:id)
+    if id == params[:id].to_i or current_user.try(:is_admin?)
+      @img = Image.find(Image.ids.sample)
+    else
+      redirect_to access_error_path
     end
   end
 
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+  def access_error
   end
 
   private
-  def set_user
-    @user = User.find(params[:id])
+
+  def is_admin
+    unless current_user.try(:is_admin?)
+      redirect_to access_error_path
+    end
   end
 
-  def user_params
-    params.require(:user).permit(:first_name,
-                                 :last_name,
-                                 :email,
-                                 :age,
-                                 :sex,
-                                 :about,
-                                 :password,
-                                 :password_confirmation,
-                                 adress_attributes: [:city,
-                                                     :street,
-                                                     :home_number,
-                                                     :zip
-                                 ]
-                                )
-  end
 end
